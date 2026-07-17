0708345e0a975057462c9c07c0822f38f43be004
Nélson Guillermo Ixcopal Cantoral <nelson.guillermo@outlook.com>
2026-07-16 21:32:42 -0600

    Se agrego console.log

--- src/app/pages/user-component/user-component.ts
+++ src/app/pages/user-component/user-component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { UserService } from '../../services/user';
 import { UserModel } from '../../models/user';
+import { ConstantPool } from '@angular/compiler';
 
 @Component({
   selector: 'app-user-component',
@@ -18,6 +19,7 @@ export class UserComponent implements OnInit{
     this.servicioUsuario.obtenerTodosUsuarios().subscribe({
       next: (datos) => {
         this.usuarios = datos;
+        console.log(datos);
       }
     })
   }
