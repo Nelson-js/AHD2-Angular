5e67b6d67f93163a1a4a7f4d7e169a84d5e1d1c3
Nélson Guillermo Ixcopal Cantoral <nelson.guillermo@outlook.com>
2026-07-16 21:18:16 -0600

    ahd2 angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './pages/user-component/user-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UserComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ahd2');
 }
---
+++ src/app/services/user.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { UserModel } from '../models/user';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  
+  private http = inject(HttpClient);
+
+  private apiUrl = "https://jsonplaceholder.typicode.com/users";
+
+  //Método para obtener todos los usuarios de la API JSONPlaceholder
+  obtenerTodosUsuarios(): Observable<UserModel[]>{
+    return this.http.get<UserModel[]>(this.apiUrl);
+  }
+
+}
